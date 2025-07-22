76d8ab4f272987a9a735bcfd31d02874f2a22479
rishithay265 <rishithay265@gmail.com>
2025-07-22 12:34:46 +0530

    Create KeyIcon.tsx

---
+++ components/icons/KeyIcon.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const KeyIcon: React.FC = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+      <path fillRule="evenodd" d="M13.5 4.938a7 7 0 1 1-9.006 4.025c.032.13.064.26.098.39l-1.488 1.487a.75.75 0 0 0 0 1.061l2.121 2.121a.75.75 0 0 0 1.06 0l1.488-1.487a6.97 6.97 0 0 1 1.042.339.75.75 0 0 0 .842-.375l.52-.951a.75.75 0 0 0-.256-.954 5.5 5.5 0 0 0-3.398-1.48V7.5a.75.75 0 0 1 1.5 0v.038a7.002 7.002 0 0 1 5.063-4.6ZM5.5 11.5a5.5 5.5 0 1 0 11 0 5.5 5.5 0 0 0-11 0Z" clipRule="evenodd" />
+    </svg>
+);
+
+export default KeyIcon;
