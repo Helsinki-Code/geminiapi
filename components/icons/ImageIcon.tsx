baef102dbd2a959be6275244733dafcec3336e1b
rishithay265 <rishithay265@gmail.com>
2025-07-22 12:34:05 +0530

    Create ImageIcon.tsx

---
+++ components/icons/ImageIcon.tsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+
+const ImageIcon: React.FC = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+    <path fillRule="evenodd" d="M1 5.25A2.25 2.25 0 0 1 3.25 3h13.5A2.25 2.25 0 0 1 19 5.25v9.5A2.25 2.25 0 0 1 16.75 17H3.25A2.25 2.25 0 0 1 1 14.75v-9.5Zm1.5 5.81v3.69c0 .414.336.75.75.75h13.5a.75.75 0 0 0 .75-.75v-2.69l-2.22-2.219a.75.75 0 0 0-1.06 0l-1.91 1.909-.48- A.75.75 0 0 0 9.99 9.75l-4.73 4.73a.75.75 0 0 0 0 1.06L9.53 19.81l-1.9-1.9a.75.75 0 0 0-1.06 0Z" clipRule="evenodd" />
+    <path d="M15.75 5.25a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0Z" />
+  </svg>
+);
+
+export default ImageIcon;
