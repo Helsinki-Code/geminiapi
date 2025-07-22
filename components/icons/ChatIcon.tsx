ba09cdec4236058411e2fc00239f526fcf62a9b3
rishithay265 <rishithay265@gmail.com>
2025-07-22 12:33:22 +0530

    Create ChatIcon.tsx

---
+++ components/icons/ChatIcon.tsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+
+const ChatIcon: React.FC = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+    <path fillRule="evenodd" d="M10 2a8 8 0 1 0 0 16 8 8 0 0 0 0-16Zm-1.25 8.75a.75.75 0 0 0 1.5 0v-2.5a.75.75 0 0 0-1.5 0v2.5ZM10 13a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM4.25 7.5a.75.75 0 0 0 0 1.5h.5a.75.75 0 0 0 0-1.5h-.5ZM15.25 7.5a.75.75 0 0 0 0 1.5h.5a.75.75 0 0 0 0-1.5h-.5Z" clipRule="evenodd" />
+    <path d="M8.25 5a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5ZM11.75 5a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5Z" />
+  </svg>
+);
+
+export default ChatIcon;
