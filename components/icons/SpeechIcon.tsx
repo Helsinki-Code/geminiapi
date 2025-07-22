e38af10f49026d7ba2750645520bcfbe2b4f7c04
rishithay265 <rishithay265@gmail.com>
2025-07-22 12:35:44 +0530

    Create SpeechIcon.tsx

---
+++ components/icons/SpeechIcon.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const SpeechIcon: React.FC = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+    <path d="M13 5a3 3 0 0 1 3 3v2a3 3 0 0 1-3 3M7 5a3 3 0 0 0-3 3v2a3 3 0 0 0 3 3" />
+    <path fillRule="evenodd" d="M10 3a1 1 0 0 1 1 1v12a1 1 0 1 1-2 0V4a1 1 0 0 1 1-1ZM3 8a1 1 0 0 1 1 1v2a1 1 0 1 1-2 0V9a1 1 0 0 1 1-1Zm14 0a1 1 0 0 1 1 1v2a1 1 0 1 1-2 0V9a1 1 0 0 1 1-1Z" clipRule="evenodd" />
+  </svg>
+);
+
+export default SpeechIcon;
