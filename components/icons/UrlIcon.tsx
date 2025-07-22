ca615de749274dd59fe0212a69dd668552ef9657
rishithay265 <rishithay265@gmail.com>
2025-07-22 12:36:21 +0530

    Create UrlIcon.tsx

---
+++ components/icons/UrlIcon.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const UrlIcon: React.FC = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+    <path fillRule="evenodd" d="M12.586 4.414a1 1 0 0 1 0 1.414L10.363 8.05a2.5 2.5 0 1 0 3.536 3.536l2.222-2.223a1 1 0 1 1 1.414 1.414l-2.475 2.475a4.5 4.5 0 0 1-6.364-6.364l2.475-2.475a1 1 0 0 1 1.414 0Zm-5.172 11.172a1 1 0 0 1 0-1.414l2.223-2.223a2.5 2.5 0 0 0-3.536-3.536L3.86 10.636a1 1 0 0 1-1.414-1.414l2.475-2.475a4.5 4.5 0 0 1 6.364 6.364L8.818 15.586a1 1 0 0 1-1.414 0Z" clipRule="evenodd" />
+  </svg>
+);
+
+export default UrlIcon;
