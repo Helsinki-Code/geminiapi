b4b7ec99b8222832a649e694d262fa51b2d12860
rishithay265 <rishithay265@gmail.com>
2025-07-22 12:34:25 +0530

    Create JsonIcon.tsx

---
+++ components/icons/JsonIcon.tsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+
+const JsonIcon: React.FC = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+    <path fillRule="evenodd" d="M10 1a9 9 0 1 0 0 18 9 9 0 0 0 0-18ZM8.24 5.235a.75.75 0 0 1 1.06 0l3.535 3.536a.75.75 0 0 1 0 1.06L9.3 13.366a.75.75 0 0 1-1.06-1.06l3.005-3.006-3.005-3.005a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
+    <path fillRule="evenodd" d="M4 10a.75.75 0 0 1 .75-.75h.01a.75.75 0 0 1 0 1.5H4.75A.75.75 0 0 1 4 10Z" clipRule="evenodd" />
+  </svg>
+);
+
+export default JsonIcon;
