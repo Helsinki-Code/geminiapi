61e9a014be3fb421d77d3b6c4ef3f184f9a1ae11
rishithay265 <rishithay265@gmail.com>
2025-07-22 12:36:43 +0530

    Create VideoIcon.tsx

---
+++ components/icons/VideoIcon.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const VideoIcon: React.FC = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+    <path d="M3.25 4A2.25 2.25 0 0 0 1 6.25v7.5A2.25 2.25 0 0 0 3.25 16h13.5A2.25 2.25 0 0 0 19 13.75v-7.5A2.25 2.25 0 0 0 16.75 4H3.25Z" />
+    <path fillRule="evenodd" d="M7.756 7.086a.75.75 0 0 1 1.11.623v4.582a.75.75 0 0 1-1.11.623l-3.04-1.824a.75.75 0 0 1 0-1.246l3.04-1.824Z" clipRule="evenodd" />
+  </svg>
+);
+
+export default VideoIcon;
