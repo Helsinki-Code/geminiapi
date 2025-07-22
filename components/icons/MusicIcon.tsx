ddf4c750afc5f75bd9a12b5b053604249811e9c3
rishithay265 <rishithay265@gmail.com>
2025-07-22 12:35:08 +0530

    Create MusicIcon.tsx

---
+++ components/icons/MusicIcon.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MusicIcon: React.FC = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+    <path d="M7 4a1 1 0 0 1 1-1h.382a1 1 0 0 1 .98.703l1.838 5.973a1 1 0 0 1-.98 1.297H9.5a1 1 0 0 1-1-1V4Z" />
+    <path fillRule="evenodd" d="M4 18.5a2.5 2.5 0 1 0 5 0 2.5 2.5 0 0 0-5 0ZM6.5 17a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z" clipRule="evenodd" />
+    <path fillRule="evenodd" d="M10.19 3.999a1 1 0 0 1 1.265-.999h.382a1 1 0 0 1 .98.703l1.838 5.973a1 1 0 0 1-.98 1.297h-.662a1 1 0 0 1-1-1V4a1 1 0 0 1 .158-.501Z" clipRule="evenodd" />
+    <path fillRule="evenodd" d="M11 15.5a2.5 2.5 0 1 0 5 0 2.5 2.5 0 0 0-5 0ZM13.5 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z" clipRule="evenodd" />
+  </svg>
+);
+
+export default MusicIcon;
