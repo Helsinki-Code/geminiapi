312be491b958a47e1b3b115c5f08b65aa3f87864
rishithay265 <rishithay265@gmail.com>
2025-07-22 12:37:02 +0530

    Create YoutubeIcon.tsx

---
+++ components/icons/YoutubeIcon.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const YoutubeIcon: React.FC = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+    <path fillRule="evenodd" d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16Zm-1.07-5.222L13.18 10 8.93 7.222v5.556Z" clipRule="evenodd" />
+  </svg>
+);
+
+export default YoutubeIcon;
